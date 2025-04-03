238f3465988d93404f383bbeb3f1b68d60d4b023
Kizenbahs <girts.kizenbahs@gmail.com>
2025-04-03 15:40:07 +0300

    Add EmailJS credentials for booking form

---
+++ src/config/email.ts
@@ -0,0 +1,11 @@
+import emailjs from '@emailjs/browser';
+
+// Initialize EmailJS
+emailjs.init('scR1M1iDqwYE75s6r');
+
+export const EMAIL_CONFIG = {
+  SERVICE_ID: 'service_88084ew',
+  TEMPLATE_ID: 'template_4pbdxzk',
+  REPLY_TO: 'info@batutufitness.lv',
+  FROM_NAME: 'Batutu Fitness'
+}; 
