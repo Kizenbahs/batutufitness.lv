9b6de23459ded26690f5f9c604b66b2f675c2f78
Kizenbahs <girts.kizenbahs@gmail.com>
2025-04-03 15:43:55 +0300

    Update notification email for testing

--- src/config/email.ts
+++ src/config/email.ts
@@ -6,6 +6,6 @@ emailjs.init('scR1M1iDqwYE75s6r');
 export const EMAIL_CONFIG = {
   SERVICE_ID: 'service_88084ew',
   TEMPLATE_ID: 'template_4pbdxzk',
-  REPLY_TO: 'info@batutufitness.lv',
+  REPLY_TO: 'girts.karcevskis@gmail.com',
   FROM_NAME: 'Batutu Fitness'
 }; 
